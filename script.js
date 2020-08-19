83959489e3aeee06a74a96d0142b12f9e803b4c9
fanni <anwarainul>
2020-08-19 21:34:33 +0700

    tugas_bintang_piramid

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = '';
+
+for( let b = 5; b > 0; b--){
+    for( let c = 0; c < b; c++ ) {
+        a += '*'
+    }
+    a += '<br>'
+    a += '\n'
+}
+document.getElementById('stars').innerHTML += a
